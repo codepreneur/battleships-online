e27f46dc13de675e402ca50c50924573ebd2181a
gomenasaai <contramundee@gmail.com>
2014-06-15 13:46:47 +0100

    added very primitive ability to drag and drop ships

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,17 @@
 require 'sinatra'
+require_relative 'coordinates'
+require_relative 'ships'
 
 set :views, Proc.new{File.join(root,'..','views')}
 set :public_folder, Proc.new{File.join(root,'..','public')}
 
-@data = []
+enable :sessions
+
+@data = {}
 
 get '/' do
 	erb :board
 end
 
-get '/place/:coord' do
 
-end
-
-get '/shoot/:coord' do
-
-end
 
